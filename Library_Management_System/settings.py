a46274548b6bab16e5745eb23a652a05af19d16b
Hr-D-LuffY <hrsiam420@gmail.com>
2024-01-08 01:31:33 +0600

    CSRF UPDATED

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://front.bluemix.net/']
+CSRF_TRUSTED_ORIGINS = ['https://hr-library.onrender.com']
 
 # Application definition
 
